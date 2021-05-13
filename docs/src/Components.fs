733821806b6008198abfc0d46c809a2c1dd633f8
Akash Mair <akash@compositional-it.com>
2021-05-13 21:58:00 +0100

    updated docs site

--- docs/src/Components.fs
+++ docs/src/Components.fs
@@ -75,57 +75,70 @@ type Components =
         let (props, setProps) = React.useState(initProps)
 
         let animationButtonConfigs =
-            [ {| Color = Components.citDarkBlue
+            [ {| Color = Components.citLightBlue
                  Hamburger = "Tilt"
-                 Updater = (fun _ -> setProps({ props with Animation = Tilt })) |}
+                 Updater = (fun _ -> setProps({ props with Animation = Tilt }))
+                 Selected = props.Animation = Tilt |}
 
               {| Color = Components.citGreen
                  Hamburger = "Squash"
-                 Updater = (fun _ -> setProps({ props with Animation = Squash })) |}
+                 Updater = (fun _ -> setProps({ props with Animation = Squash }))
+                 Selected = props.Animation = Squash |}
 
               {| Color = Components.citOrange
                  Hamburger = "Cross"
-                 Updater = (fun _ -> setProps({ props with Animation = Cross })) |}
+                 Updater = (fun _ -> setProps({ props with Animation = Cross }))
+                 Selected = props.Animation = Cross |}
 
               {| Color = Components.citYellow
                  Hamburger = "Twirl"
-                 Updater = (fun _ -> setProps({ props with Animation = Twirl })) |}
+                 Updater = (fun _ -> setProps({ props with Animation = Twirl }))
+                 Selected = props.Animation = Twirl |}
 
               {| Color = Components.citRed
                  Hamburger = "Fade"
-                 Updater = (fun _ -> setProps({ props with Animation = Fade })) |}
+                 Updater = (fun _ -> setProps({ props with Animation = Fade }))
+                 Selected = props.Animation = Fade |}
 
               {| Color = Components.citLightBlue
                  Hamburger = "Slant"
-                 Updater = (fun _ -> setProps({ props with Animation = Slant })) |}
+                 Updater = (fun _ -> setProps({ props with Animation = Slant }))
+                 Selected = props.Animation = Slant |}
 
-              {| Color = Components.citDarkBlue
+              {| Color = Components.citYellow
                  Hamburger = "Divide"
-                 Updater = (fun _ -> setProps({ props with Animation = Divide })) |}
+                 Updater = (fun _ -> setProps({ props with Animation = Divide }))
+                 Selected = props.Animation = Divide |}
 
               {| Color = Components.citGreen
                  Hamburger = "Pivot"
-                 Updater = (fun _ -> setProps({ props with Animation = Pivot })) |}
+                 Updater = (fun _ -> setProps({ props with Animation = Pivot }))
+                 Selected = props.Animation = Pivot |}
 
               {| Color = Components.citOrange
                  Hamburger = "Turn"
-                 Updater = (fun _ -> setProps({ props with Animation = Turn })) |}
+                 Updater = (fun _ -> setProps({ props with Animation = Turn }))
+                 Selected = props.Animation = Turn |}
 
               {| Color = Components.citYellow
                  Hamburger = "Sling"
-                 Updater = (fun _ -> setProps({ props with Animation = Sling })) |}
+                 Updater = (fun _ -> setProps({ props with Animation = Sling }))
+                 Selected = props.Animation = Sling |}
 
               {| Color = Components.citRed
                  Hamburger = "Squeeze"
-                 Updater = (fun _ -> setProps({ props with Animation = Squeeze })) |}
+                 Updater = (fun _ -> setProps({ props with Animation = Squeeze }))
+                 Selected = props.Animation = Squeeze |}
 
               {| Color = Components.citLightBlue
                  Hamburger = "Spin"
-                 Updater = (fun _ -> setProps({ props with Animation = Spin })) |}
+                 Updater = (fun _ -> setProps({ props with Animation = Spin }))
+                 Selected = props.Animation = Spin |}
 
-              {| Color = Components.citDarkBlue
+              {| Color = Components.citGreen
                  Hamburger = "Rotate"
-                 Updater = (fun _ -> setProps({ props with Animation = Rotate })) |} ]
+                 Updater = (fun _ -> setProps({ props with Animation = Rotate }))
+                 Selected = props.Animation = Rotate |} ]
 
         let citButton color updateProp selected =
             Html.button [
@@ -153,6 +166,7 @@ type Components =
             Html.button [
                 prop.style [
                     style.backgroundColor (if selected then color else "transparent")
+                    style.color (if selected then "white" else Components.citDarkBlue)
                     style.border(1,borderStyle.none, "")
                     style.borderRight(2, borderStyle.solid, color)
                     style.padding 8
@@ -184,19 +198,41 @@ type Components =
                 ]
             ]
 
-        let defaultButton color (buttonLabel: string) updater =
+        let defaultButton color (buttonLabel: string) updater selected =
             Html.button [
                 prop.style [
                     style.width (length.percent 100)
-                    style.backgroundColor.transparent
+                    style.backgroundColor (if selected then color else "transparent")
+                    style.color (if selected then "white" else Components.citDarkBlue)
                     style.border(2,borderStyle.solid, color)
                     style.borderRadius 5
                     style.padding 8
                     style.height 50
+                    style.fontSize 15
                     style.marginBottom 10
                 ]
                 prop.text buttonLabel
                 prop.onClick updater
+            ]
+
+        let subHeading (label: string) =
+            Html.h2 [
+                prop.style [
+                    style.borderBottom(2, borderStyle.solid, Components.citDarkBlue)
+                    style.paddingBottom 10
+                ]
+                prop.text label
+            ]
+
+        let codeBlock (code: string) =
+            Html.pre [
+                prop.style [
+                    style.padding 20
+                    style.fontSize 15
+                    style.backgroundColor "lightgrey"
+                    style.borderRadius 5
+                ]
+                prop.text code
             ]
 
         Html.div [
@@ -204,12 +240,12 @@ type Components =
                 style.display.flex
                 style.flexDirection.column
                 style.padding 50
-                style.maxWidth 1000
+                //style.maxWidth 1000
                 style.margin (0,length.auto)
             ]
             prop.children [
                 Html.div [
-                    prop.style [ style.display.flex ]
+                    prop.style [ style.display.flex; style.flexWrap.wrap ]
                     prop.children [
                         Html.div [
                             prop.style [
@@ -217,7 +253,7 @@ type Components =
 
                             ]
                             prop.children [
-                                Html.h2 "Demo"
+                                subHeading "Demo"
                                 ReactHamburger.create [
                                     ReactHamburger.hamburgerType props.Animation
                                     ReactHamburger.direction props.Direction
@@ -227,23 +263,24 @@ type Components =
                                     ReactHamburger.lineDistance props.LineDistance
                                 ]
 
-                                Html.h2 "Installation"
+                                subHeading "Installation"
+                                codeBlock
+                                        """cd ./project
+femto install Feliz.ReactHamburger"""
 
-                                Html.h2 "Sample Code"
-                                Html.pre [
-                                prop.text
+                                subHeading "Sample Code"
+                                codeBlock
                                     """
 ReactHamburger.create [
     ReactHamburger.hamburgerType Tilt
-    ReactHamburger.direction props.Direction
-    ReactHamburger.toggled props.Toggled
-    ReactHamburger.hideOutline props.HideOutLine
-    ReactHamburger.rounded props.Rounded
-    ReactHamburger.lineDistance props.LineDistance
+    ReactHamburger.direction Left
+    ReactHamburger.toggled true
+    ReactHamburger.hideOutline false
+    ReactHamburger.rounded false
+    ReactHamburger.lineDistance Small
     ]
                                     """
 
-                                ]
                             ]
                         ]
                         Html.div [
@@ -254,7 +291,7 @@ ReactHamburger.create [
                             prop.children [
                                 Html.div [
                                     prop.children [
-                                        Html.h2 "Boolean Props"
+                                        subHeading "Boolean Props"
                                         toggleButton
                                             Components.citLightBlue
                                             "Toggled"
@@ -270,7 +307,8 @@ ReactHamburger.create [
                                             "Hide outline"
                                             (fun _ -> setProps({ props with HideOutLine = not props.HideOutLine }))
                                             props.HideOutLine
-                                        Html.h2 "DU Props"
+
+                                        subHeading "DU Props"
                                         switchButtons
                                             Components.citYellow
                                             "Direction"
@@ -284,9 +322,9 @@ ReactHamburger.create [
                                               {| Name = "Medium"; Updater = (fun _ -> setProps({ props with LineDistance = Medium })); Selected = (props.LineDistance = Medium) |}
                                               {| Name = "Large"; Updater = (fun _ -> setProps({ props with LineDistance = Large })); Selected = (props.LineDistance = Large)|}]
 
-                                        Html.h2 "HamburgerType Prop"
+                                        subHeading "HamburgerType Prop"
                                         for animationButton in animationButtonConfigs do
-                                            defaultButton animationButton.Color animationButton.Hamburger animationButton.Updater
+                                            defaultButton animationButton.Color animationButton.Hamburger animationButton.Updater animationButton.Selected
 
                                     ]
                                 ]
